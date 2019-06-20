c34ebf6fc596bae5a4ca68c90474ee235b8e6344
fenghanyiqiu <woyaoaj@163.com>
2019-06-20 11:40:26 +0800

    zuoye

---
+++  作业/作业/作业/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    int i,j,x,y;
+    int a[7]={0},b[7]={0};
+    ifstream in("/Users/s20181106277/Desktop/ 作业/作业/裁判.txt");
+    
+}
