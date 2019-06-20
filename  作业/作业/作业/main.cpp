7e83f7a46a1b599f8740d51580f1276a9ee655c2
fenghanyiqiu <woyaoaj@163.com>
2019-06-20 11:48:25 +0800

    Update main.cpp

---  作业/作业/作业/main.cpp
+++  作业/作业/作业/main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int main()
 {
-    int i,j,x,y;
-    int a[7]={0},b[7]={0};
+    int average;
+    int data[7]={0};
     ifstream in("/Users/s20181106277/Desktop/ 作业/作业/裁判.txt");
     
 }
