e3c6235b914a703d9a2ea662f35ffab6526dc08f
fenghanyiqiu <woyaoaj@163.com>
2019-06-26 21:02:03 +0800

    Update main.cpp

---  作业/作业/作业/main.cpp
+++  作业/作业/作业/main.cpp
@@ -3,23 +3,26 @@
 using namespace std;
 int main()
 {
-    int average[6];//存储平均成绩；
+    int average[7];//存储平均成绩；
     int a,b,c,i,j;//用于循环;
     int x,sum=0;//用于处理数据；
-    int score[41]; //存放裁判打分；
-    string name[6];//存放裁判姓名；
-    int handle1[5],handle2[3];//用于处理数据；
-    ifstream in("/Users/s20181106277/Desktop/ 作业/作业/裁判.txt");
+    int score[42]; //存放裁判打分；
+    string name[7];//存放裁判姓名；
+    string competitor[12];//选手信息；
+    int handle1[6],handle2[4];//用于处理数据；
+    ifstream judgmentin("/Users/s20181106277/Desktop/shangchuan/ 作业/作业/裁判.txt");
+    ifstream competitorin("/Users/s20181106277/Desktop/shangchuan/ 作业/作业/选手.txt");
+    ofstream out("/Users/s20181106277/Desktop/shangchuan/ 作业/作业/裁判.txt");
     for(i=0;i<49;i++)//将数据放入数组；
     {
        if((i+1)%7==0)//将裁判名放入对应数组；
        {
            j=(i+1)/7-1;
-           in>>name[j];
+           judgmentin>>name[j];
        } else      //将打分放入对应数组；
        {
            j=(i+1)/7;
-           in>>score[i-j];
+           judgmentin>>score[i-j];
        }
     }
     for(b=0;b<7;b++)//对数据进行处理；
@@ -48,11 +51,15 @@ int main()
             {
                 handle2[a]=handle1[j];
             }
-            for(a=0;a<4;a++)
+            for(a=0;a<4;a++)//将平均数储存；
             {
                 sum=sum+handle2[a];
                 average[b]=sum/4;
             }
         }
+    }
+    for(i=0;i<12;i++)//将选手数据存入数组
+    {
+        competitorin>>competitor[i];
     }
 }
