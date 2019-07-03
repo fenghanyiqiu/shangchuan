c8362f8ce2cc97a3f6e71cd61774892b94d56e41
fenghanyiqiu <woyaoaj@163.com>
2019-07-03 19:48:47 +0800

    Update main.cpp

---  作业/作业/作业/main.cpp
+++  作业/作业/作业/main.cpp
@@ -10,34 +10,34 @@ int main()
     double average[N];                              //存储平均成绩；
     int a,b,i,j;                                    //用于循环;
     int m,n;  string y,z;  double sum,x;            //用于处理数据；
-    int score[140];                                 //存放裁判打分；
+    int score[N*7];                                 //存放裁判打分；
     string name[7];                                 //存放裁判姓名；
-    string competitor[40];                          //选手信息；
+    string competitor[N*2];                          //选手信息；
     int handle1[7],handle2[5],handle3[N];           //用于处理数据；
     ifstream judgmentin("/Users/s20181106277/Desktop/shangchuan/ 作业/作业/裁判.txt");
     ifstream competitorin("/Users/s20181106277/Desktop/shangchuan/ 作业/作业/选手.txt");
     ofstream out("/Users/s20181106277/Desktop/shangchuan/ 作业/作业/输出.txt");
-    for(i=0;i<20;i++)                                //排名需要的数；
+    for(i=0;i<N;i++)                                //排名需要的数；
     {
         rank[i]=i+1;
     }
-    for(i=0;i<20;i++)                                //为学生编号;
+    for(i=0;i<N;i++)                                //为学生编号;
     {
         number[i]=i;
     }
-    for(i=0;i<147;i++)                               //将数据放入数组；
+    for(i=0;i<N*7+7;i++)                            //将数据放入数组；
     {
-       if((i+1)%21==0)                               //将裁判名放入对应数组；
+       if((i+1)%(N+1)==0)                              //将裁判名放入对应数组；
        {
-           j=(i+1)/21-1;
+           j=(i+1)/(N+1)-1;
            judgmentin>>name[j];
        } else                                        //将打分放入对应数组；
        {
-           j=(i+1)/21;
+           j=(i+1)/(N+1);
            judgmentin>>score[i-j];
        }
     }
-    for(b=0;b<20;b++)                                 //对数据进行处理；
+    for(b=0;b<N;b++)                                 //对数据进行处理；
     {
         sum=0;
         for(i=0;i<7;i++)                             //将数据放入处理数组
@@ -66,13 +66,13 @@ int main()
         }
         average[b]=sum*1.0/5.0;
     }
-    for(i=0;i<40;i++)                                 //将选手数据存入数组；
+    for(i=0;i<2*N;i++)                                 //将选手数据存入数组；
     {
         competitorin>>competitor[i];
     }
-    for(a=0;a<20;a++)                                  //进行冒泡排序并且选手信息与分数绑定；
+    for(a=0;a<N;a++)                                   //进行冒泡排序并且选手信息与分数绑定；
     {
-        for(j=0;j<20;j++)
+        for(j=0;j<N;j++)
         {
             if(average[j]<=average[j+1])
             {
@@ -92,23 +92,23 @@ int main()
         }
     }
     out<<std::left<<setw(12)<<"排名";                   //输出排名
-    for(i=0;i<20;i++)
+    for(i=0;i<N;i++)
     {
         out<<std::left<<setw(8)<<rank[i];
     }
     out<<endl;
     out<<std::left<<setw(15)<<"学院编号";                //输出学院编号
-    for(i=0;i<20;i++)
+    for(i=0;i<N;i++)
     {
         out<<std::left<<setw(8)<<competitor[i*2];
     }
     out<<endl<<std::left<<setw(12)<<"姓名";             //输出姓名
-    for(i=0;i<20;i++)
+    for(i=0;i<N;i++)
     {
         out<<std::left<<setw(8)<<competitor[i*2+1];
     }
     out<<endl<<std::left<<setw(15)<<"最终成绩";          //输出最终成绩与裁判打分
-    for(i=0;i<20;i++)
+    for(i=0;i<N;i++)
     {
         out<<std::left<<setw(8)<<average[i];
     }
@@ -116,11 +116,11 @@ int main()
     for(i=0;i<7;i++)
     {
         out<<std::left<<setw(9)<<name[i];
-        for(j=0;j<20;j++)
+        for(j=0;j<N;j++)
         {
             handle3[j]=score[j+6*i];
         }
-        for(j=0;j<20;j++)
+        for(j=0;j<N;j++)
         {
             m=number[j];
             out<<std::left<<setw(8)<<handle3[m];
